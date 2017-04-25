0f47a1547e0f7e106e9455c3e02f9eb7617481ce
chenm <chenm91@163.com>
2017-04-25 16:12:18 +0800

    加入功能1 in feature-1

--- app/src/main/java/com/cmtech/app/thermometer/MainActivity.java
+++ app/src/main/java/com/cmtech/app/thermometer/MainActivity.java
@@ -2,12 +2,29 @@ package com.cmtech.app.thermometer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_feature_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_feature_1 = (Button) findViewById(R.id.btn_feature_1);
+        btn_feature_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btn_feature_1.getText().toString().equals("功能1")) {
+                    btn_feature_1.setText("无功能1");
+                } else {
+                    btn_feature_1.setText("功能1");
+                }
+            }
+        });
+
     }
 }
